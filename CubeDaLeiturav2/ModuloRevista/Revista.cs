ea204a3d533fdebd2a1bdf68ae6901c3fc168882
Thiago Culau <thiago.culau1@outlook.com>
2024-05-10 11:58:17 -0300

    Finalizado Revista

--- CubeDaLeiturav2/ModuloRevista/Revista.cs
+++ CubeDaLeiturav2/ModuloRevista/Revista.cs
@@ -5,12 +5,14 @@
         public string Titulo { get; set; }
         public string Numero { get; set; }
         public string DataAno { get; set; }
+        public bool EstaReservada { get; set; }
 
-        public Revista(string titulo, string numero, string dataAno)
+        public Revista(string titulo, string numero, string dataAno, bool estaReservada)
         {
             Titulo = titulo;
             Numero = numero;
             DataAno = dataAno;
+            EstaReservada = estaReservada;
         }
     }
 }
--- CubeDaLeiturav2/ModuloRevista/TelaRevista.cs
+++ CubeDaLeiturav2/ModuloRevista/TelaRevista.cs
@@ -17,8 +17,9 @@ namespace CubeDaLeiturav2.ModuloRevista
             string numero = Console.ReadLine();
             Console.Write("\nAno: ");
             string dataAno = Console.ReadLine();
+            bool estaReservada = false;
 
-            Revista revista = new Revista(titulo, numero, dataAno);
+            Revista revista = new Revista(titulo, numero, dataAno, estaReservada);
 
             RRevista.Salvar(revista);
 
@@ -26,12 +27,54 @@ namespace CubeDaLeiturav2.ModuloRevista
         }
         public void Editar()
         {
+            Revista[] revista = RRevista.Leitura();
+            tela.Cabecalho();
+            Console.Write("\n\tSelecionar Revista");
 
+            for (int i = 0; i < revista.Length; i++)
+            {
+                Console.Write($"\nID {i} | Titulo {revista[i].Titulo}");
+            }
+            int oprevista = tela.LerInt("\nDigite a ID da revista que voce deseja editar: ");
+
+            tela.Cabecalho();
+
+            Console.Write("\n\tEditar amigo");
+            Console.Write($"\nAtualmente o nome está cadastrado como {revista[oprevista].Titulo}. Digite o novo nome: ");
+            revista[oprevista].Titulo = Console.ReadLine();
+            Console.Write($"\nAtualmente o numero da edição cadastrado é {revista[oprevista].Numero}. Digite o novo numero da edição: ");
+            revista[oprevista].Numero = Console.ReadLine();
+            Console.Write($"\nAtualmente o ano da edição cadastrado é {revista[oprevista].DataAno}. Digite o novo ano da edição: ");
+            revista[oprevista].DataAno = Console.ReadLine();
+
+            tela.CadastroComSucesso();
         }
-
         public void Reservas()
         {
+            Revista[] revista = RRevista.Leitura();
+            tela.Cabecalho();
+            Console.Write("\n\tSelecionar Revista");
 
+            for (int i = 0; i < revista.Length; i++)
+            {
+                Console.Write($"\nID {i} | Titulo {revista[i].Titulo}");
+            }
+            while (true)
+            {
+                int idrevista = tela.LerInt("\nDigite a ID da revista que voce deseja reservar?: ");
+                if (idrevista < 0 || idrevista > revista.Length)
+                {
+                    continue;
+                }
+                revista[idrevista].EstaReservada = true;
+                RRevista.Salvar(revista[idrevista]);
+                break;
+            }
+
+            tela.Cabecalho();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Reserva efetuada com sucesso!\n\nPrecione qualquer tecla para continuar.");
+            Console.ResetColor();
         }
     }
 }
